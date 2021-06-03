6e8ddda0b2c337f6203f5fd67c4524e7c70c2cc2
Nicolas Alberto <darkathnva@gmail.com>
2021-06-03 20:58:39 -0300

    Adicionado atributos a model de Jogos

--- src/app/models/jogo.ts
+++ src/app/models/jogo.ts
@@ -1,8 +1,19 @@
+import { Genero } from './genero';
 export class Jogo {
   id?: number;
   nome: string;
+  sinopse: string;
+  generos: Array<Genero>;
+  nota: number;
+  estado: string;
+  review: string;
 
-  constructor(nome: string){
+  constructor(nome: string, sinopse: string, generos: Array<Genero>, nota: number, estado: string, review: string){
     this.nome = nome;
+    this.sinopse = sinopse;
+    this.generos = generos;
+    this.nota = nota;
+    this.estado = estado;
+    this.review = review;
   }
 }
