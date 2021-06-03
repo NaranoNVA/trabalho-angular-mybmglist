9be21d71102e70d56f2fae3350ffad8e122544e7
Nicolas Alberto <darkathnva@gmail.com>
2021-06-03 19:43:56 -0300

    Criação dos componentes, serviços e models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LivroComponent } from './views/livro/livro.component';
+import { JogoComponent } from './views/jogo/jogo.component';
+import { FilmeComponent } from './views/filme/filme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LivroComponent,
+    JogoComponent,
+    FilmeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/models/jogo.ts
@@ -0,0 +1,8 @@
+export class Jogo {
+  id?: number;
+  nome: string;
+
+  constructor(nome: string){
+    this.nome = nome;
+  }
+}
