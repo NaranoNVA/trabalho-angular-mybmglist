595c42d5bee10500a7c018fc5b88f544a457569f
Lucas <lucas.ssantos192@gmail.com>
2021-06-03 22:34:39 -0300

    Inicio de crud de jogos

--- src/app/services/jogo.service.ts
+++ src/app/services/jogo.service.ts
@@ -1,9 +1,20 @@
+import { environment } from './../../environments/environment';
+import { Jogo } from './../models/jogo';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class JogoService {
 
-  constructor() { }
+  endereco = `${environment.baseUrl}jogos`;
+
+  constructor(private http: HttpClient) { }
+
+  listar(): Observable<Jogo[]>{
+    return this.http.get<Jogo[]>(this.endereco);
+  }
 }
--- src/app/views/jogo/jogo.component.ts
+++ src/app/views/jogo/jogo.component.ts
@@ -1,4 +1,9 @@
+import { JogoService } from './../../services/jogo.service';
+import { Jogo } from 'src/app/models/jogo';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-jogo',
@@ -7,9 +12,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JogoComponent implements OnInit {
 
-  constructor() { }
+  jogos = new Array<Jogo>();
+  jogo?: Jogo;
+  colunas = ['nome', 'sinopse', 'generos', 'estado', 'action'];
+  jogoSelecionado?: Jogo = undefined;
+  inserindo = false;
+
+  constructor(private jogoService: JogoService, private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
+    this.listar();
   }
 
+  mostrarSnackBar(msg: string): void{
+    this.snackBar.open(msg, '', {duration: 3000});
+  }
+
+  listar(): void{
+    this.jogoService.listar().subscribe(
+      jogos => {
+        this.jogos = jogos;
+        console.log(this.jogos);
+      },
+      error => {
+        const e = error as HttpErrorResponse;
+        console.log(e);
+        this.mostrarSnackBar('Erro ao listar os jogos! Tente novamente mais tarde!');
+      });
+  }
+  
+  private handleServiceError(error: any): void{
+    const e = error as HttpErrorResponse;
+    console.log(e);
+    this.mostrarSnackBar(e.statusText);
+  }
+
+  selecionar(jogo: Jogo) {
+    this.jogoSelecionado = jogo;
+    this.inserindo = false;
+  }
+
+  cancelar() {
+    this.jogoSelecionado = undefined;
+    this.listar();
+  }
+
+  salvar() {
+    if (this.inserindo){
+      this.inserir();
+    } else {
+      this.atualizar();
+    }
+  }
+
+  private inserir(){
+    this.jogoService.inserir(this.jogoSelecionado).subscribe(() => {
+      this.cancelar();
+      this.mostrarSnackBar('Usuário inserido com sucesso');
+    },
+    error => {
+      this.handleServiceError(error);
+    });
+}
 }
